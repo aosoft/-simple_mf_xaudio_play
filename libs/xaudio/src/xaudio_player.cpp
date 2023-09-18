ff5174badee0a736c43d03647059f1a57150e1ee
Yasuhiro Taniuchi <tan-y@aosoft.jp>
2023-09-19 00:42:24 +0900

    Added initialization and finalization methods in the xaudio_player class

--- libs/xaudio/src/xaudio_player.cpp
+++ libs/xaudio/src/xaudio_player.cpp
@@ -8,4 +8,31 @@ xaudio_player::xaudio_player()
 
 xaudio_player::~xaudio_player()
 {
+    finalize();
 }
+
+HRESULT xaudio_player::initialize(const WAVEFORMATEX& wfx)
+{
+    finalize();
+    CHECK_HR(XAudio2Create(&_xaudio, 0, XAUDIO2_DEFAULT_PROCESSOR))
+    CHECK_HR(_xaudio->CreateMasteringVoice(&_mastering_voice, XAUDIO2_DEFAULT_CHANNELS, XAUDIO2_DEFAULT_SAMPLERATE, 0, nullptr, nullptr))
+    CHECK_HR(_xaudio->CreateSourceVoice(&_source_voice, &wfx, 0, XAUDIO2_DEFAULT_FREQ_RATIO, this, nullptr, nullptr))
+    CHECK_HR(_source_voice->SetVolume(1.0f))
+
+    return S_OK;
+}
+
+void xaudio_player::finalize()
+{
+    _mastering_voice = nullptr;
+    _source_voice = nullptr;
+    _xaudio = nullptr;
+}
+
+void STDMETHODCALLTYPE xaudio_player::OnVoiceProcessingPassStart(UINT32 BytesRequired) { }
+void STDMETHODCALLTYPE xaudio_player::OnVoiceProcessingPassEnd() { }
+void STDMETHODCALLTYPE xaudio_player::OnStreamEnd() { }
+void STDMETHODCALLTYPE xaudio_player::OnBufferStart(void* pBufferContext) { }
+void STDMETHODCALLTYPE xaudio_player::OnBufferEnd(void* pBufferContext) { }
+void STDMETHODCALLTYPE xaudio_player::OnLoopEnd(void* pBufferContext) { }
+void STDMETHODCALLTYPE xaudio_player::OnVoiceError(void* pBufferContext, HRESULT Error) { }
--- libs/xaudio/src/xaudio_player.h
+++ libs/xaudio/src/xaudio_player.h
@@ -3,7 +3,7 @@
 #include "common.h"
 #include <xaudio2.h>
 
-class xaudio_player : public IXAudio2VoiceCallback {
+class xaudio_player : private IXAudio2VoiceCallback {
 private:
     com_ptr<IXAudio2> _xaudio;
     IXAudio2MasteringVoice* _mastering_voice;
@@ -12,4 +12,16 @@ private:
 public:
     xaudio_player();
     ~xaudio_player();
+
+    HRESULT initialize(const WAVEFORMATEX& wfx);
+    void finalize();
+
+private:
+    void STDMETHODCALLTYPE OnVoiceProcessingPassStart(UINT32 BytesRequired) override;
+    void STDMETHODCALLTYPE OnVoiceProcessingPassEnd() override;
+    void STDMETHODCALLTYPE OnStreamEnd() override;
+    void STDMETHODCALLTYPE OnBufferStart(void* pBufferContext) override;
+    void STDMETHODCALLTYPE OnBufferEnd(void* pBufferContext) override;
+    void STDMETHODCALLTYPE OnLoopEnd(void* pBufferContext) override;
+    void STDMETHODCALLTYPE OnVoiceError(void* pBufferContext, HRESULT Error) override;
 };
