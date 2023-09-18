1cdf71d2d01dddf7f45982f43c2c0a695a299df5
Yasuhiro Taniuchi <tan-y@aosoft.jp>
2023-09-19 08:54:42 +0900

    Update xaudio_player class with new methods

--- libs/xaudio/src/xaudio_player.cpp
+++ libs/xaudio/src/xaudio_player.cpp
@@ -17,7 +17,7 @@ HRESULT xaudio_player::initialize(const WAVEFORMATEX& wfx)
     CHECK_HR(XAudio2Create(&_xaudio, 0, XAUDIO2_DEFAULT_PROCESSOR))
     CHECK_HR(_xaudio->CreateMasteringVoice(&_mastering_voice, XAUDIO2_DEFAULT_CHANNELS, XAUDIO2_DEFAULT_SAMPLERATE, 0, nullptr, nullptr))
     CHECK_HR(_xaudio->CreateSourceVoice(&_source_voice, &wfx, 0, XAUDIO2_DEFAULT_FREQ_RATIO, this, nullptr, nullptr))
-    CHECK_HR(_source_voice->SetVolume(1.0f))
+    CHECK_HR(set_volume(1.0f))
 
     return S_OK;
 }
@@ -27,6 +27,44 @@ void xaudio_player::finalize()
     _mastering_voice = nullptr;
     _source_voice = nullptr;
     _xaudio = nullptr;
+}
+
+HRESULT xaudio_player::start()
+{
+    return S_OK;
+}
+
+HRESULT xaudio_player::stop()
+{
+    return S_OK;
+}
+
+XAUDIO2_VOICE_STATE xaudio_player::get_voice_state() const
+{
+    if (is_initialized()) {
+        XAUDIO2_VOICE_STATE ret;
+        _source_voice->GetState(&ret);
+        return ret;
+    }
+    return {};
+}
+
+float xaudio_player::get_volume() const
+{
+    if (is_initialized()) {
+        float volume;
+        _source_voice->GetVolume(&volume);
+        return volume;
+    }
+    return 0.0f;
+}
+
+HRESULT xaudio_player::set_volume(float volume)
+{
+    if (is_initialized()) {
+        return _source_voice->SetVolume(volume);
+    }
+    return S_FALSE;
 }
 
 void STDMETHODCALLTYPE xaudio_player::OnVoiceProcessingPassStart(UINT32 BytesRequired) { }
--- libs/xaudio/src/xaudio_player.h
+++ libs/xaudio/src/xaudio_player.h
@@ -16,6 +16,18 @@ public:
     HRESULT initialize(const WAVEFORMATEX& wfx);
     void finalize();
 
+    [[nodiscard]] bool is_initialized() const
+    {
+        return _xaudio != nullptr && _mastering_voice != nullptr && _source_voice != nullptr;
+    }
+
+    HRESULT start();
+    HRESULT stop();
+
+    [[nodiscard]] XAUDIO2_VOICE_STATE get_voice_state() const;
+    [[nodiscard]] float get_volume() const;
+    HRESULT set_volume(float volume);
+
 private:
     void STDMETHODCALLTYPE OnVoiceProcessingPassStart(UINT32 BytesRequired) override;
     void STDMETHODCALLTYPE OnVoiceProcessingPassEnd() override;
